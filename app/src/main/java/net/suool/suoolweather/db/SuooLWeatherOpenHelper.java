88ed885424de6184d11ffc51073562c1b56868fb
SuooL <1020935219@qq.com>
2014-10-03 20:37:59 +0800

    Second commit

---
+++ app/src/main/java/net/suool/suoolweather/db/SuooLWeatherOpenHelper.java
@@ -0,0 +1,7 @@
+package net.suool.suoolweather.db;
+
+/**
+ * Created by SuooL on 2014/10/3.
+ */
+public class SuooLWeatherOpenHelper {
+}
---
+++ app/src/main/java/net/suool/suoolweather/model/County.java
@@ -0,0 +1,7 @@
+package net.suool.suoolweather.model;
+
+/**
+ * Created by SuooL on 2014/10/3.
+ */
+public class County {
+}
